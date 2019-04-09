99f5c78448e766a66d4ff68f64fe88afb8ec3908
Marci Papineau <marcidenmark@me.com>
2019-04-09 12:23:42 +0200

    first commit

---
+++ python_intro.py
@@ -0,0 +1,71 @@
+# print('Hello, Django girls!')
+#
+# if 3>2:
+# 	print('It works!')
+
+# if 5>2:
+# 	print('5 is indeed greater than2')
+# else:
+# 	print('5 is not greater than 2')
+
+
+# name ='Marci'
+# if name =='Jens':
+# 	print('Hey Jens!')
+# elif name == 'Marci':
+# 	print('Hey Marci!')
+# else:
+# 	print('Hey anonymous!')
+
+#Change the volume if it's too loud or too quite
+# volume = 80
+# if volume < 20:
+# 	print("It's kind of quite.")
+# elif 20<=volume < 40:
+# 	print("It's nice for background music")
+# elif 40<=volume<60:
+# 	print("Perfect, I can hear all the details")
+# elif 60<= volume <80:
+# 	print("Nice for parties")
+# elif 80<=volume<100:
+# 	print("A bit loud!")
+# else:
+# 	print("My ears are hurting! :(")
+
+# def hi():
+# 	print('Hi there!')
+# 	print('How are you doing?')
+
+# hi()
+
+# def hi(name):
+# 	if name == 'Marci':
+# 		print('Hi Marci')
+# 	elif name == 'Jens':
+# 		print('Hi Jens!')
+# 	else:
+# 		print('Hi anonymous')
+# hi("Jens")
+#
+#
+def hi(name):
+	print('Hi ' + name + '!')
+
+# hi('Albi and Andre')
+
+
+# girls = ['marci ', 'erica ', 'verona' ,'maria']
+
+# for name in girls:
+# 	hi(name)
+# 	print('Next girl')
+
+for i in range(1,6):
+	print(i)
+
+
+
+
+
+
+
